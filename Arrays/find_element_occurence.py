a07f1294335c4f49d554828087e2ec8881803756
rohitchauhan10 <49388888+rohitchauhan10@users.noreply.github.com>
2021-08-04 15:06:14 +0530

    Add files via upload

---
+++ Arrays/find_element_occurence.py
@@ -0,0 +1,38 @@
+
+# Given an array arr[] and size of array is n and one another key x, and give you a segment size k.
+# The task is to find that the key x present in every segment of size k in arr[].
+
+                    # python solution
+
+def element_occurence(arr,x,k,n):
+    i = 0
+    while i < n :
+        j = 0 
+        while j < k:
+            if arr[i+j]==x:
+                break
+            j = j+1
+        if j == k :
+            return False
+
+        i = i + k
+    if i == n :
+        return True
+
+    j = i - k
+    while j < n:
+        if arr[j]== x:
+            break
+        j = j + 1
+    if j == n :
+        return True
+
+arr = [1,3,4,3,6,4,7,8,3,10,1,3]
+x = 3
+k = 3
+n = len(arr)
+
+if(element_occurence(arr,x,k,n)):
+    print("yes element is present")
+else :
+    print("element not present")
